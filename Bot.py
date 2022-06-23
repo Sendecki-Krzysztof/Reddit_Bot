36958dfce0cb8c7efa707fdccb5c0a3e2ceaa026
Krzysztof-Sendecki <sendecki.krzysztof19@gmail.com>
2022-06-23 17:52:35 -0500

    Added code to screenshot posts and comments

--- Bot.py
+++ Bot.py
@@ -1,6 +1,21 @@
 import praw
 from praw.models import MoreComments
 from gtts import gTTS
+from playwright.sync_api import sync_playwright
+
+
+def screenshotPost(currentNum, givenPost='', givenComment=''):
+    with sync_playwright() as p:
+        browser = p.firefox.launch()
+        page = browser.new_page()
+        page.set_viewport_size({'width': 1920, 'height': 1080})
+        page.goto("https://www.reddit.com/" + givenPost)
+        url = page.url
+        page.goto(url + givenComment)
+        image = str(currentNum) + '.png'
+        page.screenshot(path=image)
+        browser.close()
+
 
 testBot = praw.Reddit(client_id='k1nwIrUWW716xJZpguPS1Q',
                       client_secret='_UEvS23EyGaIv12OUXXhSzXfLhp2vw',
@@ -10,8 +25,15 @@ subreddit = testBot.subreddit('AskReddit')
 commentList = []
 for post in subreddit.hot(limit=1):
     postTitle = post.title
+    print(postTitle)
+    print(post.id)
+    screenshotPost(-1, post.id)
     currentComment = 0
+
     for comment in post.comments:
+        if currentComment == 1:
+            print(comment.id)
+            screenshotPost(currentComment, post.id, comment.id)
         if isinstance(comment, MoreComments):
             continue
         if len(comment.body) > 20:
@@ -20,5 +42,5 @@ for post in subreddit.hot(limit=1):
 
 print(commentList[0])
 
-textToSpeech = gTTS(text=commentList[0], lang='en')
+textToSpeech = gTTS(text=(commentList[0]), lang='en')
 textToSpeech.save('test.mp3')
