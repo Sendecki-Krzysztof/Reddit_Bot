5c84ed7e82149ba25c8eb9187f5c3babb6c04528
Krzysztof-Sendecki <sendecki.krzysztof19@gmail.com>
2022-06-23 20:03:55 -0500

    generates audio and screenshots to go with it

--- Bot.py
+++ Bot.py
@@ -1,14 +1,17 @@
+from pathlib import Path
 import praw
 from praw.models import MoreComments
 from gtts import gTTS
 from playwright.sync_api import sync_playwright
+from mutagen.mp3 import MP3
 
 testBot = praw.Reddit(client_id='k1nwIrUWW716xJZpguPS1Q',
                       client_secret='_UEvS23EyGaIv12OUXXhSzXfLhp2vw',
                       user_agent='<console:MangoBot:0.1>')
 
-postTitle = ""
 subreddit = testBot.subreddit('AskReddit')
+redditPost = None
+videoLength = 0
 commentList = []
 screenshotList = []
 audioList = []
@@ -46,23 +49,42 @@ def getComments(post):
         if comment.body in ["[removed]", "[deleted]"]:
             continue
         if len(comment.body) > 20:
-            commentList.append(comment.body)
+            commentList.append(comment)
             currentComment += 1
 
 
+# Processes the Posts
 def getPost():
+    print("Getting Reddit Posts...")
     for post in subreddit.hot(limit=1):
-        global postTitle
-        postTitle = post.title
+        global redditPost
+        redditPost = post
 
         # screenshots the title of post
         screenshot(-1, post.id)
         getComments(post)
 
+    print("Got reddit Posts...")
+
 
 getPost()
-print(commentList[0])
-print(postTitle)
+# makes sure dir exist...
+Path("audio").mkdir(parents=True, exist_ok=True)
+Path("images").mkdir(parents=True, exist_ok=True)
 
-textToSpeech = gTTS(text=(commentList[0]), lang='en')
-textToSpeech.save('test.mp3')
+print("creating audio and screenshots...")
+titleMP3 = gTTS(text=redditPost.title, lang='en')
+titleMP3.save('./audio/title.mp3')
+total = 0
+for comment in commentList:
+    if videoLength <= 50:
+        audioList.append(gTTS(text=commentList[total].body, lang='en'))
+        screenshot(total, redditPost.id, commentList[total].id)
+        audioFile = './audio/' + str(total) + '.mp3'
+        audioList[total].save(audioFile)
+        videoLength += MP3(audioFile).info.length
+        print(videoLength)
+        total += 1
+print("got audio and screenshots...")
+
+print("done!")
